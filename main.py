ba50403cb6d0bcc00103313889a7ae248f2f3f98
snipeso <code@sophia.science>
2020-01-09 23:16:08 +0100

    implement chronometer

--- main.py
+++ main.py
@@ -53,6 +53,29 @@ logging.info('Initialization completed')
 # screen.show_cue("START")
 # core.wait(CONF["timing"]["cue"])
 
+
+class Chronometer:
+    def __init__(self, name, treshold):
+        self.name = name
+        self.old = 0
+        self.countNOK = 0
+        self.countTOT = 0
+        self.treshold = treshold
+
+    def lap(self):
+        now = time.time()
+        diff = now-self.old
+        self.countTOT += 1
+        if diff > self.treshold and self.countTOT:
+            self.countNOK += 1
+            freq = self.countNOK / self.countTOT
+            print(f'{self.name} took {diff}s. It happened with a frequency of {freq}')
+        self.old = now
+
+    # chrono1 = Chronometer("keyboardresponse", 0.001)
+    # chrono1.lap()
+
+
 sequence_number = 0
 mainTimer = core.CountdownTimer(CONF["task"]["duration"])
 

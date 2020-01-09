c05c3cc692effcf96016dc5e048530496b0bb595
snipeso <code@sophia.science>
2020-01-09 22:49:30 +0100

    Implement hack to show digits based on position

--- screen.py
+++ screen.py
@@ -57,32 +57,27 @@ class Screen:
         fonts = fm.getFontFamilyStyles()
 
         # self.counter = visual.TextStim(self.window)
-        def makeTextBox(i):
-            return visual.TextBox(window=self.window,
-                                  #   font_size=21,
-                                  #   font_name=fonts[0][0],
-                                  # border_color=[-1, -1, 1],
-                                  #   grid_color=[-1, -1, 1],
-                                  textgrid_shape=(1, 1),
-                                  #   #   align_horz='left',
-                                  #   #   align_vert='bottom',
-                                  #   grid_stroke_width=1,
-                                  #textgrid_shape=[20, 4],
-                                  font_color=[1, 1, 1],
-                                  size=(0.2, 0.2),
-                                  #   pos=(0.0, 0),
-                                  #   grid_horz_justification='center',
-                                  #   units='norm',
-                                  font_size=41,
-                                  pos=(0.25 * i - 0.5, 0.25),
-                                  grid_horz_justification='center',
-                                  grid_vert_justification='center',
-                                  #   units='norm',
-                                  )
-        self.DIGITS_COUNT = 4
-        self.counter = []
-        for x in range(self.DIGITS_COUNT):
-            self.counter.append(makeTextBox(x))
+        self.counter = visual.TextBox(window=self.window,
+                                      #   font_size=21,
+                                      #   font_name=fonts[0][0],
+                                      # border_color=[-1, -1, 1],
+                                      grid_color=[-1, -1, 1],
+                                      textgrid_shape=(10, 1),
+                                      #   #   align_horz='left',
+                                      #   #   align_vert='bottom',
+                                      #   grid_stroke_width=1,
+                                      #textgrid_shape=[20, 4],
+                                      font_color=[1, 1, 1],
+                                      size=(1, 1),
+                                      #   pos=(0.0, 0),
+                                      #   grid_horz_justification='center',
+                                      #   units='norm',
+                                      font_size=41,
+                                      pos=(0, 0.25),
+                                      grid_horz_justification='center',
+                                      grid_vert_justification='center',
+                                      #   units='norm',
+                                      )
 
     def show_overview(self):
         # self.counter.draw()
@@ -113,38 +108,32 @@ class Screen:
         self.window.flip()
 
     def start_countdown(self):
-        # self.counter.color = "white"
         self.show_counter(0)
-        # self.counter.setText("0")
-        # self.counter.draw()
+        self.set_counter_color("white")
         self.window.flip()  # TODO: Here, run script for trigger and saving start time
 
-    def show_counter(self, time):
-        text = "{:4}".format(round(1000*time))
-        for pos, character in enumerate(text[-self.DIGITS_COUNT:]):
-            self.counter[pos].setText(character)
-            self.counter[pos].draw()
-        # self.counter.setText(text)
-        # self.counter.draw()
-        # self.window.flip()
+    def show_counter(self, time, colored=False):
+        speed = round(1000*time)
+        text = "{}   ".format(speed)  # hack to show the relevant digits
+        self.counter.setText(text)
+        if colored:
+            if speed < self.CONF["task"]["minTime"]:
+                self.set_counter_color(self.CONF["task"]["earlyColor"])
+            elif speed < self.CONF["task"]["maxTime"]:
+                self.set_counter_color(self.CONF["task"]["victoryColor"])
+            else:
+                self.set_counter_color(self.CONF["task"]["lateColor"])
+
+        self.counter.draw()
+        return speed
 
     def set_counter_color(self, color):
-        for c in self.counter:
-            c.color = color
+        self.counter.setFontColor(color)
 
     def show_result(self, time):
         # gives different color stimulus depending on result
-        speed = round(1000*time)
-        self.show_counter(speed)
+        speed = self.show_counter(time, colored=True)
         # self.counter.setText(str(speed))
-        if speed < self.CONF["task"]["minTime"]:
-            self.set_counter_color(self.CONF["task"]["earlyColor"])
-            # self.counter.color = self.CONF["task"]["earlyColor"]
-        elif speed < self.CONF["task"]["maxTime"]:
-            self.set_counter_color(self.CONF["task"]["victoryColor"])
-            # self.counter.color = self.CONF["task"]["victoryColor"]
-        else:
-            self.set_counter_color(self.CONF["task"]["lateColor"])
-            # self.counter.color = self.CONF["task"]["lateColor"]
+
         self.counter.draw()
         self.window.flip()

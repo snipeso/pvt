5ef44cc0fe941995997fd77e356480b5b718a95c
snipeso <code@sophia.science>
2020-01-10 16:20:46 +0100

    detailed comments

--- main.py
+++ main.py
@@ -31,6 +31,10 @@ logging.info('Initialization completed')
 
 #########################################################################
 
+##############
+# Introduction
+##############
+
 # Display overview of session
 screen.show_overview()
 core.wait(CONF["timing"]["overview"])
@@ -51,35 +55,44 @@ logging.info('Starting blank period')
 core.wait(CONF["timing"]["rest"])
 # TODO: send end wait trigger
 
-# Start main experiment
+# Cue start of the experiment
 screen.show_cue("START")
 core.wait(CONF["timing"]["cue"])
 
+##########################################################################
+
+#################
+# Main experiment
+#################
 
 sequence_number = 0
 mainTimer = core.CountdownTimer(CONF["task"]["duration"])
 
-
 while mainTimer.getTime() > 0:
 
+    # log
     sequence_number += 1
     logging.info('Starting iteration #%s', sequence_number)
 
-    screen.show_fixation_box()
-    datalog["sequence_number"] = sequence_number
-
+    ###############################
     # Wait a random period of time
+
     delay = random.uniform(
         CONF["fixation"]["minDelay"], CONF["fixation"]["maxDelay"])
 
+    # log
+    datalog["sequence_number"] = sequence_number
     datalog["delay"] = delay
     logging.info('Starting delay of %s seconds', delay)
 
+    # start
     delayTimer = core.CountdownTimer(delay)
+    screen.show_fixation_box()
 
-    # Record any extra key presses during wait
     extraKeys = []
     while delayTimer.getTime() > 0:
+
+        # Record any extra key presses during wait
         extraKey = kb.getKeys()
         if extraKey:
             if extraKey[0].name == 'q':
@@ -94,16 +107,18 @@ while mainTimer.getTime() > 0:
             screen.flash_fixation_box(CONF["fixation"]["fillColor"])
 
         core.wait(0.0005)
-    datalog["extrakeypresses"] = extraKeys
+
+    #######################
+    # Stimulus presentation
 
     # initialize stopwatch
     Timer = core.Clock()
     keys = []
     Skipped = False
-    datalog["startTime"] = mainClock.getTime()
 
-    def onFlip():
+    def onFlip():  # TODO: does this go somewhere else?
         kb.clock.reset()
+        datalog["startTime"] = mainClock.getTime()
         # TODO: send trigger
 
     # run stopwatch
@@ -113,9 +128,14 @@ while mainTimer.getTime() > 0:
         keys = kb.getKeys(waitRelease=False)
         screen.show_counter(Timer.getTime())
         screen.window.flip()
+
+        # end if no answer comes in time
         if Timer.getTime() > CONF["task"]["warningTime"]:
             Skipped = True
             break
+
+    #########
+    # Outcome
 
     if Skipped:
         # Alarm.play()
@@ -132,11 +152,16 @@ while mainTimer.getTime() > 0:
             logging.warning('Forced quit during task')
             sys.exit(3)
 
-        # save to file
+        # save to memory
         datalog["rt"] = reactionTime
         datalog["response_key"] = keys[0].name
 
+    # save data to file
+    datalog["extrakeypresses"] = extraKeys
     datalog.flush()
+
+###########
+# Concluion
 
 # End main experiment
 screen.show_cue("DONE!")

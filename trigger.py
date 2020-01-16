dc46d45572b1e737ce938152136caeda2c2b37b7
snipeso <snipeso@users.noreply.github.com>
2020-01-16 19:04:15 +0000

    Fix trigger system, commit 3 of 3

--- trigger.py
+++ trigger.py
@@ -10,26 +10,28 @@ class Trigger:
         format='%(asctime)s-%(levelname)s-%(message)s',
         )  # This is a log for debugging the script, and prints messages to the terminal
 
-        self._delay = 0.01
+        self._delay = 0.03
         self._labels = labels
 
-        try:
-            self._port = serial.Serial(serial_device)
+        self._port = serial.Serial(serial_device)
+        # try:
             
-        except:
-            logging.warning("NO USB THINGY FOR TRIGGERS!")
+        # except:
+        #     logging.warning("NO USB THINGY FOR TRIGGERS!")
 
     def _write(self, n):
-        self._port._write([n])
+        self._port.write([n])
         time.sleep(self._delay)
+        self._port.write([0x00])
        
 
     def send(self, name):
         #TODO: if name not in list, throw error
-        try:
             self._write(self._labels[name])
-        except:
-            logging.info("pretend sent trigger %s", name)
+        # try:
+        # except:
+        #     logging.info("pretend sent trigger %s", name)
+        #     raise KeyError
 
     def reset(self):
         self._write(0xFF)

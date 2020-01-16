ddcca966a0c4786f397f31c071e7ea65b1b712c8
EliasMeier <eliaspeter.meier@gmx.ch>
2020-01-16 11:35:46 +0100

    made experiment work without trigger cable, and added bash instructions

--- trigger.py
+++ trigger.py
@@ -1,22 +1,37 @@
 import serial
 import time
+import logging
 
 
 class Trigger:
     def __init__(self, serial_device, labels={}):
-        self._port = serial.Serial(serial_device)
+        logging.basicConfig(
+        level=logging.WARNING,
+        format='%(asctime)s-%(levelname)s-%(message)s',
+        )  # This is a log for debugging the script, and prints messages to the terminal
+
         self._delay = 0.01
         self._labels = labels
+        
+        try:
+            self._port = serial.Serial(serial_device)
+            
+        except:
+            logging.warning("NO USB THINGY FOR TRIGGERS!")
 
-    def write(self, n):
-        self._port.write([n])
+    def _write(self, n):
+        self._port._write([n])
         time.sleep(self._delay)
+       
 
     def send_trigger(self, name):
-        self.write(self._labels[name])
+        try:
+            self._write(self._labels[name])
+        except:
+            logging.info("pretend sent trigger %s", name)
 
     def reset(self):
-        self.write(0xFF)
+        self._write(0xFF)
 
 
 # t = Trigger("/dev/ttyUSB0", {
